db85c15cea8f8e4d160498bde84fbdf105397a04
PsqNew <balahonov.dmitry@gmail.com>
2019-03-22 08:02:26 +0300

    Add: add solution Task "Brackets"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let breckets_main = [];
+    let bracket = Array.from(str);
+    let result = true;
+    if (bracket.length%2 !== 0) {
+        return false;
+    }
+    bracket.forEach(function (mainBracket) {
+        bracketsConfig.forEach(function(checkBracket, i) {
+            if (checkBracket[0] === mainBracket && checkBracket[0] !== checkBracket[1]) {
+                breckets_main.push(i);
+            }
+            else if (checkBracket[0] === mainBracket && checkBracket[0] === checkBracket[1]) {
+                breckets_main.push(i);
+            }
+            else if (checkBracket[1] === mainBracket && breckets_main[breckets_main.length-1] === i) {
+                breckets_main.pop();
+            }
+        });
+    });
+    if (breckets_main.length > 0){
+        result = false;
+    }
+    return result;
+};
